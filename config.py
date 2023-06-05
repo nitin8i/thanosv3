8476a6b218b300f08700f95febb7f8c45a96a27d
Divyansh Jindal <132134813+nitin8i@users.noreply.github.com>
2023-06-05 22:51:43 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,25 @@
+from sample_config import Config
+
+
+class Development(Config):
+    # get this values from the my.telegram.org
+    APP_ID = 10248430
+    API_HASH = "42396a6ff14a569b9d59931643897d0d"
+    # the name to display in your alive message
+    ALIVE_NAME = "rishabh"
+    # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
+    DB_URI = "postgres://hwtxqqfc:U_QHjE6TByrb8MVJOuEcfDod9FXa1BkN@satao.db.elephantsql.com/hwtxqqfc"
+    # After cloning the repo and installing requirements do python3 stringsetup.py an fill that value with this
+    STRING_SESSION ="1BVtsOKUBuxHEs2U4zz8VGUPnjsVqL9lELiGGrCMjdUYNcJWoLpD1LhrqXgQ47iGWpQQGiTAzFJ99ptfE3WSarqNywrYihqVMXt0off_NgojSV4gQ7Et1H721nDwTrsEGlW1bIaMacgZv9MORQYBdX3eD4_vZSunaqNAUSOG4C_HSpJSnIQKbH_F8oA0sSVqBH42clUzgjXU3Y8Rx9NTiwN5jPky7SbEN6EoZ3M_DmiJWmxFS8m6ahY08p3DJsrTjnTlOLvWIiW-kRss6PFmCbcbc9kRGSSOtyOoz-veyIpcenMwVKQNpgCC2wktNPvkDFJJBGLda4bSbhvUFn6Yf0ArJ8A4HzU4=" # "1BVtsOGgBu21dLz241cIdffngb3vF5b400tG2ARIv9j48fNjbCn_HLblpx8UeAyzUNsR7RPjx3z_QNbDHfGgsyRYQe7EMwJ8HhHUGbjJuVbHAomifZuIFoPFEYbngy0pHRgyMQylOg4iYIInczI0onBnoeK-7PfbD0mLU3EHrV8D87UgbgrK7jKpajOPJ0sM5RfG5C1AVP__6UErvqy65B23Z-CS5wWRiuMpvMEI9-bsalbqeMleorC49tycwOC0Ab1e5WOteqKS-S0grhBWFXubc6QNYDLW_3VxJFWOkAU6sxG9IlMoBH2KQ8ERKeaeLxOCKK0VkqyFnvTyWSMSbWCrClwvZM4M="
+    # create a new bot in @botfather and fill the following vales with bottoken
+    TG_BOT_TOKEN = "6150758905:AAGj-b8f1GYcxGGJCGikgNA4PKRek2vSqEA"
+    # create a private group and a rose bot to it and type /id and paste that id here (replace that -100 with that group id)
+    PRIVATE_GROUP_BOT_API_ID = -1001635414973
+    # command handler
+    COMMAND_HAND_LER = "."
+    # command hanler for sudo
+    SUDO_COMMAND_HAND_LER = "."
+    # External plugins repo
+    EXTERNAL_REPO = "https://github.com/rishabhanand2/tha_plugins"
+    #don't edit this 
+    THANOSABUSE = "False"#don't edit this
